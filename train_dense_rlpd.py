2e606037409055b03f6ec42b2a864f03437e6bd7
dashora7 <nrdashora@gmail.com>
2024-05-07 15:37:14 -0700

    dense training path

--- train_dense_rlpd.py
+++ train_dense_rlpd.py
@@ -87,7 +87,7 @@ def main(_):
         env_name_alt = "microwave"
         goalname = "microwave"
         # max_path_length = 50
-        goalpath = "/home/dashora7/franka_misc_data/microwave_img.png"
+        goalpath = "/home/dashora7/franka_misc_data/dibya_custom_micro_goal.png"
     elif FLAGS.env_name == "KitchenSlideCabinetV0":
         env_name_alt = "slidecabinet"
         goalname = "slide cabinet"
@@ -100,11 +100,11 @@ def main(_):
     import gym
     pixel_keys = ('image',)
     envname = "kitchen-" + env_name_alt + "-v0"
-    env = gym.make(envname)
+    env = gym.make(envname, control_mode='joint_velocity')
     env = RecordEpisodeStatistics(env, deque_size=1)
     env.seed(FLAGS.seed)
     
-    eval_env = gym.make(envname)
+    eval_env = gym.make(envname, control_mode='joint_velocity')
     eval_env = TimeLimit(eval_env)
     eval_env.seed(FLAGS.seed + 42)
 
@@ -155,7 +155,7 @@ def main(_):
         # value_def = ICVFViT(encoder_def, icvf_def)
         
         vf_def = ensemblize(MonolithicVF, 2)(hidden_dims)
-        encoder_def = encoders['atari']()
+        encoder_def = encoders['resnetv2-26-1-128']()
         value_def = ICVFWithEncoder(encoder_def, vf_def)
         
         icvf_agent = learner.create_learner(
